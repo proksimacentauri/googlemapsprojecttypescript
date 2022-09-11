757bdac53bc7d81851e229bb7bf9dfe4b4bb5792
Donna <donnasoujeh@gmail.com>
2022-09-11 21:54:21 +0200

    remove unneccesary console.log

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { LoadScript } from '@react-google-maps/api';
 import Map from './components/Map';
 
 function App() {
-  console.log(process.env.REACT_APP_GOOGLE_MAPS_KEY)
   return (
     <div className="App">
       <h3>Map Project</h3>
